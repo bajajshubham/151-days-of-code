062be96ad1bb44f194207d36777e3d1f7a8ace99
shubhambajaj <shubhambajaj32@gmail.com>
2021-01-06 21:47:19 +0530

    feat: day3 03_functions read+exercise done

--- day-2/03_functions/main.js
+++ day-2/03_functions/main.js
@@ -37,3 +37,64 @@ function printFarmInventory()
     zeroPaddedLabeling();
 }
 
+// Your code here.
+function min(number1,number2)
+{
+ 	return number1 < number2 ? number1 : number2 ;	 
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+// Your code here.
+function isEven(num)
+{
+  	//console.log("init-entry:",num);
+  	if ( num < 0  ) return false;
+ 	if ( num == 0 ) return true;
+  	if ( num == 1 ) return false;
+  	//console.log("entry-2",num-2);
+  	return isEven(num-2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+// Your code here.
+function countCharacter(astring,characterToBeCounted)
+{
+  let count = 0;
+  for(let xi=0; xi<astring.length; ++xi){
+  	if(astring[xi]===characterToBeCounted) ++count;
+  }
+  return count;
+}
+
+function countChar(astring,characterToBeCounted)
+{
+	return countCharacter(astring,characterToBeCounted);
+}
+
+//console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
+// author's code
+function isEven(n)
+{
+    if ( n == 0 )      return true;
+    else if ( n == 1 ) return false;
+    else if ( n <  0)  return isEven(-n);
+    else               return isEven(n-2);
+}
+
+
+
+
